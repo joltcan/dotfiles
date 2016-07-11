9680ccce44cef89553e628efe551a87f89e7d781
Fredrik Lundhag <f@mekk.com>
2016-07-11 12:27:25 +0200

    lets try slock instead of xautolock

--- scripts/lock_script.sh
+++ scripts/lock_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 xset -dpms
 xset -display :0.0 dpms force off 
-i3lock -c 101010 -i ~/Dropbox/Bilder/BG/pluto_bg.png 
+slock
+#i3lock -c 531222 -i ~/Dropbox/Bilder/BG/pluto_bg.png 
 
