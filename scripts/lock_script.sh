f50ea1c837c686d5adba373862ea8204f956c366
Fredrik Lundhag <f@mekk.com>
2016-10-13 15:48:25 +0200

    new screen, hooray!

--- scripts/lock_script.sh
+++ scripts/lock_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-xset -dpms
-xset -display :0.0 dpms force off 
-slock
+#xset -dpms
+#xset -display :0.0 dpms force off 
+#slock
 #i3lock -c 531222 -i ~/Dropbox/Bilder/BG/pluto_bg.png 
 
--- scripts/toggle_screen.sh
+++ scripts/toggle_screen.sh
@@ -10,13 +10,16 @@
 
 # Screen names, as shown with xrandr
 IN="eDP1"
-EXT="DP2-2"
+EXT1="DP2-1"
+EXT2="DP2-2"
 
-if (xrandr | grep "$EXT disconnected"); then
-	xrandr --output $EXT --off --output $IN --auto
+if (xrandr | grep "$EXT1 disconnected"); then
+	xrandr --output $EXT1 --off --output $EXT2 --off --output $IN --auto
     # lock just in case
-    /usr/local/bin/lock_script.sh &
+	xautolock -locknow
 else
-	xrandr --output $IN --auto --output $EXT --auto --left-of $IN --primary
+	xrandr --output $EXT1 --auto --primary --output $EXT2 --auto --left-of $EXT1
+	#xrandr --output $IN --auto --output $EXT1 --auto --left-of $IN --primary
+	#xrandr --output $EXT2 --auto --left-of $EXT1
 fi
 
